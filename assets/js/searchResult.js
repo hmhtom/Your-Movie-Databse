6e4c8113b89950ab3ebb1321224bd19e1c3d3d30
hmhtom <hmhtom@gmail.com>
2022-09-29 20:54:15 -0400

    update html/css style

--- assets/js/searchResult.js
+++ assets/js/searchResult.js
@@ -32,25 +32,33 @@ function renderSearch(data) {
   $("#searchResultQuery").append(` and found ${data.results.length} results.`);
   //For each result create card
   for (var i in data.results) {
-    let card = $(`<div class="col s6 m4 l3 xl2 card-container">
-    <div class="card medium hoverable user-select-none">
-      <div class="card-image">
-        <img 
-        src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}" 
-        onerror="this.src='https://via.placeholder.com/300?text=YMDB'"
-        alt="poster"/>
-      </div>
-      <div class="card-content">
-        <span class="card-main-title grey-text text-darken-4">${data.results[i].title}</span>
-      </div>
-      <div class="card-action">
-          <a href="static-movie-page.html?id=${data.results[i].id}">View Detail</a>
+    let card = $(`
+    <div class="col s6 m4 l3 xl2 card-container">
+      <div class="card medium hoverable user-select-none">
+        <div class="card-image">
+          <img 
+            src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}" 
+            onerror="this.src='https://via.placeholder.com/300?text=YMDB'"
+            alt="poster"/>
+        </div>
+        <div class="card-content">
+          <span class="card-main-title grey-text text-darken-4">${
+            data.results[i].title
+          }</span>
+        </div>
+        <div class="card-action">
+          <a href="static-movie-page.html?id=${
+            data.results[i].id
+          }&title=${encodeURIComponent(data.results[i].title)}">View Detail</a>
           <i class="material-icons right activator">more_vert</i>
+        </div>
+        <div class="card-reveal">
+          <span class="card-title">${
+            data.results[i].title
+          }<i class="material-icons right">close</i></span>
+          <p>${data.results[i].overview}</p>
+        </div>
       </div>
-    <div class="card-reveal">
-      <span class="card-title">${data.results[i].title}<i class="material-icons right">close</i></span>
-      <p>${data.results[i].overview}</p>
-    </div>
     </div>`);
     card.attr({
       "data-title": `${data.results[i].title}`,
